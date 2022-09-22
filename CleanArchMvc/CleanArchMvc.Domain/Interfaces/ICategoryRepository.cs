b543bc966cda86a74d4fb627723fab4fa7251e9e
Wagner Belarmino Souza <wfbelarmino@gmail.com>
2022-09-22 15:37:13 -0300

    adicionado o projeto até o capitulo 35

---
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    #region ///Método Assincrono
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCatories(); // Assinatura de método, que retorna uma lista de categoria.
+        Task<Category> GetById(int? id); // Assinatura de método, onde uso o Id que retorna uma categoria.
+
+        Task<Category> Create(Category category); //Criando categoria
+        Task<Category> Update(Category category); //Atualizando categoria
+        Task<Category> Remove(Category category); //Removendo categoria
+
+        //O Task significa de a Interface são assincronas. 
+    }  
+#endregion 
+}
---
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetCatories(); // Assinatura de método, que retorna uma lista de Produto.
+        Task<Product> GetByIdAsync(int? id); // Assinatura de método, Retona um Produto pelo ID.
+        Task<Product> GetProductCategoryAsync(int? id); // Assinatura de método, Retornando um Produto pelo Id de uma categoria.
+
+        Task<Product> Create(Product Product); //Criando categoria
+        Task<Product> Update(Product Product); //Atualizando categoria
+        Task<Product> Remove(Product Product); //Removendo categoria
+
+        //O Task significa de a Interface são assincronas. 
+    }
+}
